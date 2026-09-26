55696b1067cd814833e1611def38f68fb1096e02
Parikshit Shukla <parikshitshukla30@gmail.com>
2026-09-26 11:56:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,17 +3,13 @@ const savedLang = localStorage.getItem("language") || "en";
 
 function setLanguage(lang) {
     localStorage.setItem("language", lang);
-
     document.querySelectorAll("[data-en][data-hi]").forEach(el => {
         el.textContent = el.dataset[lang];
     });
-
     document.documentElement.lang = lang;
-
     document.querySelectorAll(".language-btn").forEach(btn =>
         btn.classList.remove("active")
     );
-
     document.querySelector(
         `.language-btn[onclick="setLanguage('${lang}')"]`
     )?.classList.add("active");
